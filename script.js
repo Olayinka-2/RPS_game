a47e0d1877a837e462f78a6da2ecb0f0d6961746
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-06-02 19:52:15 +0100

    feat: Add the human choice
    This commit allows the human to input his own choice in the game

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ function getComputerChoice(){
    let option = ["Rock", "Paper", "Scissors"];
    return option[Math.ceil(Math.random() * 3) - 1]
 }
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
+
+function getHumanChoice(){
+   let choice = prompt("Input your choice here. ");
+
+   if(choice === "Rock" ||
+      choice === "Paper" ||
+      choice === "Scissors"
+   ) {
+      return choice
+   } else {
+      return "Input the right choice"
+   }
+}
+
+// console.log(getHumanChoice())
